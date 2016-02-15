dee326904b49476f95676b9acac98d4ed5836516
robert <robertwojnar@mail.com>
2016-02-15 22:11:08 +0100

    First commit

---
+++ MvvmDemo1.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace MvvmDemo1.WPF.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        public string Title => "Main window";
+        public ICommand LoadedCommand => new RelayCommand(Loaded);
+
+        private void Loaded()
+        {
+            Debug.WriteLine("Loaded");
+        }
+    }
+}
